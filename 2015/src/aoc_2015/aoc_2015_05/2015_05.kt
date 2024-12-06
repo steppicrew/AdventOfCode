71c37143a649a6d1116c9018a579fa2f1ba39343
stephan hantigk <s.hantigk@runlevel3.de>
2024-12-06 23:05:17 +0100

    Solved days 5 and 6

---
+++ 2015/src/aoc_2015/aoc_2015_05/2015_05.kt
@@ -0,0 +1,40 @@
+package aoc_2015.aoc_2015_05
+
+import aoc_2015.tools.simpleIO
+
+const val YEAR = 2015
+const val DAY = 5
+
+val EXPECTED_RESULTS = listOf(
+    1 to (2 to 0),
+    0 to (255 to 55),
+)
+
+
+fun run1(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): Int {
+    val vowels = setOf('a', 'e', 'i', 'u', 'o')
+    val reForbidden = """ab|cd|pq|xy""".toRegex()
+    val reTwice = """(\w)\1""".toRegex()
+    return lines
+        .count { line ->
+            line.count {
+                vowels.contains(it)
+            } >= 3 &&
+                    !reForbidden.containsMatchIn(line) &&
+                    reTwice.containsMatchIn(line)
+        }
+}
+
+fun run2(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): Int {
+    val reRepeat1 = """(\w\w).*\1""".toRegex()
+    val reRepeat2 = """(\w).\1""".toRegex()
+
+    return lines
+        .count { line ->
+            reRepeat1.containsMatchIn(line) && reRepeat2.containsMatchIn(line)
+        }
+}
+
+fun main() {
+    simpleIO(YEAR, DAY, ::run1 to ::run2, EXPECTED_RESULTS)
+}
---
+++ 2015/src/aoc_2015/aoc_2015_06/2015_06.kt
@@ -0,0 +1,87 @@
+package aoc_2015.aoc_2015_06
+
+import aoc_2015.tools.simpleIO
+import kotlin.math.max
+
+const val YEAR = 2015
+const val DAY = 6
+
+val EXPECTED_RESULTS = listOf(
+    0 to (377891 to 14110788)
+)
+
+
+fun run1(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): Int {
+    val map = mutableSetOf<Pair<Int, Int>>()
+
+    val reLine = """(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)""".toRegex()
+
+    fun turnOn(xy: Pair<Int, Int>) {
+        map.add(xy)
+    }
+
+    fun turnOff(xy: Pair<Int, Int>) {
+        map.remove(xy)
+    }
+
+    fun toggle(xy: Pair<Int, Int>) {
+        if (!map.add(xy)) {
+            map.remove(xy)
+        }
+    }
+
+    lines.asSequence()
+        .mapNotNull { reLine.matchEntire(it) }
+        .forEach { match ->
+            val (command, fromX, fromY, toX, toY) = match.destructured
+            val fn = when (command) {
+                "turn on" -> ::turnOn
+                "turn off" -> ::turnOff
+                "toggle" -> ::toggle
+                else -> throw RuntimeException("This should not happen")
+            }
+            (fromX.toInt()..toX.toInt()).forEach { x ->
+                (fromY.toInt()..toY.toInt()).forEach { y -> fn(x to y) }
+            }
+        }
+    return map.size
+}
+
+fun run2(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): Int {
+    val map = mutableMapOf<Pair<Int, Int>, Int>()
+
+    val reLine = """(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)""".toRegex()
+
+    fun turnOn(xy: Pair<Int, Int>) {
+        map.compute(xy) { key, oldValue -> (oldValue ?: 0) + 1 }
+    }
+
+    fun turnOff(xy: Pair<Int, Int>) {
+        map.computeIfPresent(xy) { key, oldValue -> max(oldValue - 1, 0) }
+    }
+
+    fun toggle(xy: Pair<Int, Int>) {
+        map.compute(xy) { key, oldValue -> (oldValue ?: 0) + 2 }
+    }
+
+    lines.asSequence()
+        .mapNotNull { reLine.matchEntire(it) }
+        .forEach { match ->
+            val (command, fromX, fromY, toX, toY) = match.destructured
+
+            val fn = when (command) {
+                "turn on" -> ::turnOn
+                "turn off" -> ::turnOff
+                "toggle" -> ::toggle
+                else -> throw RuntimeException("This should not happen")
+            }
+            (fromX.toInt()..toX.toInt()).forEach { x ->
+                (fromY.toInt()..toY.toInt()).forEach { y -> fn(x to y) }
+            }
+        }
+    return map.values.sum()
+}
+
+fun main() {
+    simpleIO(YEAR, DAY, ::run1 to ::run2, EXPECTED_RESULTS)
+}
