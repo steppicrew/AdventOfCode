b709fe32575c66db4446b19ab15f421548e46e67
stephan hantigk <s.hantigk@runlevel3.de>
2024-12-07 06:41:10 +0100

    Solved day 7

--- 2024/src/aoc_2024/aoc_2024_07/2024_07.kt
+++ 2024/src/aoc_2024/aoc_2024_07/2024_07.kt
@@ -8,18 +8,54 @@ const val DAY = 7
 
 // ref to (run1 to run2)
 // values may by of any type, null is for 'not known' and write result into file
-val EXPECTED_RESULTS: ExpectedRefResults<Int> = listOf(
-    1 to (null to null),
-    0 to (null to null),
+val EXPECTED_RESULTS: ExpectedRefResults<ULong> = listOf(
+    1 to (3749UL to 11387UL),
+    0 to (882304362421UL to 145149066755184UL),
 )
 
 
-fun run1(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): Int {
-    return lines.count()
+fun run1(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): ULong {
+    val rePart = """(\d+): ([\d ]+)""".toRegex()
+    val equations = lines
+        .mapNotNull { rePart.matchEntire(it) }
+        .map { match ->
+            val (result, operators) = match.destructured
+            result.toULong() to operators.split(" ").map(String::toULong)
+        }
+
+    fun getResults(operators: MutableList<ULong>): Set<ULong> {
+        val operator = operators.removeLast()
+        if (operators.isEmpty()) return setOf(operator)
+        return getResults(operators.subList(0, operators.size)).flatMap {
+            listOf(it + operator, it * operator)
+        }.toSet()
+    }
+
+    return equations.filter { (result, operators) ->
+        getResults(operators.toMutableList()).any { result == it }
+    }.sumOf { it.first }
 }
 
-fun run2(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): Int {
-    return lines.count()
+fun run2(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): ULong {
+    val rePart = """(\d+): ([\d ]+)""".toRegex()
+    val equations = lines
+        .mapNotNull { rePart.matchEntire(it) }
+        .map { match ->
+            val (result, operators) = match.destructured
+            result.toULong() to operators.split(" ").map(String::toULong)
+        }
+
+    fun getResults(operators: MutableList<ULong>): Set<ULong> {
+        val operator = operators.removeLast()
+        if (operators.isEmpty()) return setOf(operator)
+        return getResults(operators.subList(0, operators.size)).flatMap {
+            listOf(it + operator, it * operator, (it.toString() + operator.toString()).toULong())
+        }.toSet()
+    }
+
+    return equations.filter { (result, operators) ->
+        getResults(operators.toMutableList()).any { result == it }
+    }.sumOf { it.first }
 }
 
 fun main() {
