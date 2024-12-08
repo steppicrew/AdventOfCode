0fcdeabbb6c4d9c0dfe5f35d3b47d2a52eb70c04
stephan hantigk <s.hantigk@runlevel3.de>
2024-12-08 17:52:51 +0100

    Prepared days 9 to 25

---
+++ 2024/src/aoc_2024/aoc_2024_17/2024_17.kt
@@ -0,0 +1,29 @@
+package aoc_2024.aoc_2024_17
+
+import aoc_2024.tools.ExpectedRefResults
+import aoc_2024.tools.simpleIO
+
+const val YEAR = 2024
+const val DAY = 17
+
+typealias ResultType = Int
+
+// ref to (run1 to run2)
+// values may be of any type, null is for 'not known' and write result into file
+val EXPECTED_RESULTS: ExpectedRefResults<ResultType> = listOf(
+    1 to (null to null),
+    0 to (null to null),
+)
+
+
+fun run1(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): ResultType {
+    return lines.count()
+}
+
+fun run2(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): ResultType {
+    return lines.count()
+}
+
+fun main() {
+    simpleIO(YEAR, DAY, ::run1 to ::run2, EXPECTED_RESULTS)
+}
