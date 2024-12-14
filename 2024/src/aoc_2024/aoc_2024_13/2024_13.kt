d098e3f7f682d2e716f7b3e32fad6a5f94243740
stephan hantigk <s.hantigk@runlevel3.de>
2024-12-14 07:17:17 +0100

    Solved day 14

--- 2024/src/aoc_2024/aoc_2024_13/2024_13.kt
+++ 2024/src/aoc_2024/aoc_2024_13/2024_13.kt
@@ -18,83 +18,83 @@ val EXPECTED_RESULTS: ExpectedRefResults<ResultType> = listOf(
 typealias Pos = Pair<Long, Long>
 
 fun run1(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): ResultType {
-    val aTokens = 3
-    val bTokens = 1
+    val tokensA = 3
+    val tokensB = 1
 
     val reButtonA = """Button A: X\+(\d+), Y\+(\d+)""".toRegex()
     val reButtonB = """Button B: X\+(\d+), Y\+(\d+)""".toRegex()
-    val rePrize = """Prize: X=(\d+), Y=(\d+)""".toRegex()
+    val rePrizePosition = """Prize: X=(\d+), Y=(\d+)""".toRegex()
 
-    fun find(a: Pos, b: Pos, result: Pos): Long? {
+    fun neededTokens(a: Pos, b: Pos, result: Pos): Long {
         val factorA = (result.first * b.second - result.second * b.first) / (a.first * b.second - a.second * b.first)
         val factorB = (result.first - factorA * a.first) / b.first
         return if (
             result.first == factorA * a.first + factorB * b.first &&
             result.second == factorA * a.second + factorB * b.second
         ) {
-            factorA * aTokens + factorB * bTokens
+            factorA * tokensA + factorB * tokensB
         } else {
-            null
+            0L
         }
     }
 
     return lines.chunked(4).sumOf { chunk ->
         val aMatch = reButtonA.matchEntire(chunk.first())
         val bMatch = reButtonB.matchEntire(chunk[1])
-        val prizeMatch = rePrize.matchEntire(chunk[2])
+        val prizeMatch = rePrizePosition.matchEntire(chunk[2])
         if (aMatch == null || bMatch == null || prizeMatch == null) {
-            0
+            0L
         } else {
             val (buttonAX, buttonAY) = aMatch.destructured.toList().map(String::toLong)
             val (buttonBX, buttonBY) = bMatch.destructured.toList().map(String::toLong)
             val (prizeX, prizeY) = prizeMatch.destructured.toList().map(String::toLong)
 
-            find(
+            neededTokens(
                 buttonAX to buttonAY,
                 buttonBX to buttonBY,
                 prizeX to prizeY
-            ) ?: 0
+            )
         }
     }
 }
 
 fun run2(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): ResultType {
-    val aTokens = 3
-    val bTokens = 1
+    val tokensA = 3
+    val tokensB = 1
 
     val reButtonA = """Button A: X\+(\d+), Y\+(\d+)""".toRegex()
     val reButtonB = """Button B: X\+(\d+), Y\+(\d+)""".toRegex()
-    val rePrize = """Prize: X=(\d+), Y=(\d+)""".toRegex()
+    val rePrizePosition = """Prize: X=(\d+), Y=(\d+)""".toRegex()
 
-    fun find(a: Pos, b: Pos, result: Pos): Long? {
+    fun neededTokens(a: Pos, b: Pos, result: Pos): Long {
         val factorA = (result.first * b.second - result.second * b.first) / (a.first * b.second - a.second * b.first)
         val factorB = (result.first - factorA * a.first) / b.first
         return if (
             result.first == factorA * a.first + factorB * b.first &&
             result.second == factorA * a.second + factorB * b.second
         ) {
-            factorA * aTokens + factorB * bTokens
+            factorA * tokensA + factorB * tokensB
         } else {
-            null
+            0L
         }
     }
 
     return lines.chunked(4).sumOf { chunk ->
         val aMatch = reButtonA.matchEntire(chunk.first())
         val bMatch = reButtonB.matchEntire(chunk[1])
-        val prizeMatch = rePrize.matchEntire(chunk[2])
+        val prizeMatch = rePrizePosition.matchEntire(chunk[2])
         if (aMatch == null || bMatch == null || prizeMatch == null) {
-            0
+            0L
         } else {
             val (buttonAX, buttonAY) = aMatch.destructured.toList().map(String::toLong)
             val (buttonBX, buttonBY) = bMatch.destructured.toList().map(String::toLong)
             val (prizeX, prizeY) = prizeMatch.destructured.toList().map { it.toLong() + 10000000000000L }
 
-            find(
+            neededTokens(
                 buttonAX to buttonAY,
                 buttonBX to buttonBY,
                 prizeX to prizeY
-            ) ?: 0
+            )
         }
     }
 }
--- 2024/src/aoc_2024/aoc_2024_14/2024_14.kt
+++ 2024/src/aoc_2024/aoc_2024_14/2024_14.kt
@@ -11,17 +11,79 @@ typealias ResultType = Int
 // ref to (run1 to run2)
 // values may be of any type, null is for 'not known' and write result into file
 val EXPECTED_RESULTS: ExpectedRefResults<ResultType> = listOf(
-    1 to (null to null),
-    0 to (null to null),
+    // 2 to (null to null),
+    1 to (12 to null),
+    0 to (231852216 to 8159),
 )
 
 
 fun run1(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): ResultType {
-    return lines.count()
+    val reRobot = """p=(\d+),(\d+) v=(-?\d+),(-?\d+)""".toRegex()
+    val robots = lines.mapNotNull { reRobot.matchEntire(it) }.map { match ->
+        val (startPosX, startPosY, speedX, speedY) = match.destructured.toList().map(String::toInt)
+        (startPosX to startPosY) to (speedX to speedY)
+    }
+
+    // val size = 11 to 7
+    val size = 101 to 103
+
+    val time = 100
+
+    val endPositions = robots
+        .map { (startPos, speed) ->
+            (startPos.first + speed.first * time) % size.first to (startPos.second + speed.second * time) % size.second
+        }
+        .map { (if (it.first < 0) it.first + size.first else it.first) to (if (it.second < 0) it.second + size.second else it.second) }
+        .groupingBy { it }.eachCount().filterValues { it > 0 }
+
+    val quadrantBorder = (size.first) / 2 to (size.second) / 2
+
+    val quadrants = endPositions
+        .filter { (position, _) -> position.first != quadrantBorder.first && position.second != quadrantBorder.second }
+        .map { (position, count) ->
+            ((if (position.first < quadrantBorder.first) 0 else 1) to (if (position.second < quadrantBorder.second) 0 else 1)) to count
+        }.groupBy({ it.first }, { it.second }).mapValues { it.value.sum() }
+
+    println("$quadrantBorder -> $quadrants")
+
+    return quadrants.values.reduce { prod, count -> prod * count }
 }
 
-fun run2(lines: List<String>, @Suppress("UNUSED_PARAMETER") log: (String) -> Unit): ResultType {
-    return lines.count()
+fun run2(lines: List<String>, log: (String) -> Unit): ResultType {
+    val reRobot = """p=(\d+),(\d+) v=(-?\d+),(-?\d+)""".toRegex()
+    val robots = lines.mapNotNull { reRobot.matchEntire(it) }.map { match ->
+        val (startPosX, startPosY, speedX, speedY) = match.destructured.toList().map(String::toInt)
+        (startPosX to startPosY) to (speedX to speedY)
+    }
+
+    // val size = 11 to 7
+    val size = 101 to 103
+
+    val neighbours = sequenceOf(
+        1 to 0, 1 to 1, 0 to 1, -1 to 1, -1 to 0, -1 to -1, 0 to -1, 1 to -1
+    )
+
+    (1..100000).asSequence().forEach { time ->
+        val endPositions = robots
+            .map { (startPos, speed) ->
+                (startPos.first + speed.first * time) % size.first to (startPos.second + speed.second * time) % size.second
+            }
+            .map { (if (it.first < 0) it.first + size.first else it.first) to (if (it.second < 0) it.second + size.second else it.second) }
+            .toSet()
+
+        val connected = endPositions.filter { position ->
+            neighbours.any { endPositions.contains(it.first + position.first to it.second + position.second) }
+        }
+
+        if (connected.size > lines.size / 2) {
+            log("Time: $time")
+            (0..<size.second).asSequence().forEach { line ->
+                log((0..<size.first).joinToString("") { if (endPositions.contains(it to line)) "#" else " " })
+            }
+        }
+    }
+
+    return 0
 }
 
 fun main() {
