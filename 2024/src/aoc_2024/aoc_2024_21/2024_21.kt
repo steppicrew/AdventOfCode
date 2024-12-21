b8a5aeb7209b3468e71062db29a7123653472a6e
stephan hantigk <s.hantigk@runlevel3.de>
2024-12-21 20:46:52 +0100

    Finished day 21

--- 2024/src/aoc_2024/aoc_2024_21/2024_21.kt
+++ 2024/src/aoc_2024/aoc_2024_21/2024_21.kt
@@ -3,26 +3,231 @@ package aoc_2024.aoc_2024_21
 import aoc_2024.tools.ExpectedRefResults
 import aoc_2024.tools.InputData
 import aoc_2024.tools.simpleIO
+import java.util.*
 
 const val YEAR = 2024
 const val DAY = 21
 
-typealias ResultType = Int
+typealias ResultType = Long
 
 // ref to (run1 to run2)
 // values may be of any type, null is for 'not known' and write result into file
 val EXPECTED_RESULTS: ExpectedRefResults<ResultType> = listOf(
-    1 to (null to null),
-    0 to (null to null),
+    1 to (126384L to 154115708116294L),
+    0 to (246990L to 306335137543664L),
 )
 
+val codeKeypad = mapOf(
+    'A' to (2 to 3),
+    '0' to (1 to 3),
+    '1' to (0 to 2),
+    '2' to (1 to 2),
+    '3' to (2 to 2),
+    '4' to (0 to 1),
+    '5' to (1 to 1),
+    '6' to (2 to 1),
+    '7' to (0 to 0),
+    '8' to (1 to 0),
+    '9' to (2 to 0),
+)
+val directionalKeypad = mapOf(
+    'A' to (2 to 0),
+    '^' to (1 to 0),
+    '<' to (0 to 1),
+    'v' to (1 to 1),
+    '>' to (2 to 1),
+)
 
 fun run1(input: InputData): ResultType {
-    return input.lines.count()
+
+    val robotStartPositions = mapOf(
+        0 to (2 to 3),
+        1 to (2 to 0),
+        2 to (2 to 0),
+    )
+
+    val robotArms = robotStartPositions.toMutableMap()
+
+    fun getKeyPad(robot: Int): Map<Char, Pair<Int, Int>> {
+        if (robot == 0) return codeKeypad
+        return directionalKeypad
+    }
+
+    val directions = listOf((1 to 0) to ">", (0 to 1) to "v", (-1 to 0) to "<", (0 to -1) to "^")
+
+    val navigationCache = mutableMapOf<Pair<Int, Pair<Pair<Int, Int>, Pair<Int, Int>>>, List<String>>()
+    fun navigate(robot: Int, start: Pair<Int, Int>, end: Pair<Int, Int>): List<String> {
+        return navigationCache.getOrPut(robot to (start to end)) {
+            val costs = getKeyPad(robot).values.associateWith { Int.MAX_VALUE }.toMutableMap()
+            val ways = mutableMapOf<Pair<Int, Int>, Set<String>>()
+            val queue = PriorityQueue<Pair<Int, Int>> { p1, p2 -> costs[p1]!! - costs[p2]!! }
+            queue.add(start)
+            costs[start] = 0
+            ways[start] = setOf("")
+            while (queue.isNotEmpty()) {
+                val pos = queue.remove()
+                val cost = costs[pos]!!
+                val way = ways[pos]!!
+                directions.map { (pos.first + it.first.first to pos.second + it.first.second) to it.second }
+                    .filter { costs.containsKey(it.first) }
+                    .forEach { (nextPos, direction) ->
+                        val nextCost = costs[nextPos]!!
+                        if (cost + 1 > nextCost) return@forEach
+                        if (cost + 1 < nextCost) {
+                            costs[nextPos] = cost + 1
+                            ways[nextPos] = way.map { it + direction }.toSet()
+                            queue.add(nextPos)
+                        } else {
+                            ways[nextPos] = ways[nextPos]!! + way.map { it + direction }.toSet()
+                        }
+                    }
+
+            }
+            ways[end]!!.toList()
+        }
+    }
+
+    fun pressButton2(robot: Int, button: Char): List<String> {
+        if (robot == 3) return listOf(button.toString())
+        val keyPad = getKeyPad(robot)
+        val position = robotArms[robot]!!
+        val destination = keyPad[button]!!
+        val movements = navigate(robot, position, destination).map { it + "A" }
+        robotArms[robot] = destination
+
+        fun combine(possibilities: List<List<String>>): List<String> {
+            val firstList = possibilities.first()
+            if (possibilities.size == 1) return firstList
+            return firstList.flatMap { letter ->
+                combine(possibilities.drop(1)).map { letter + it }
+            }
+        }
+
+        return movements.flatMap { movement ->
+            val x = combine(movement.map { letter ->
+                pressButton2(robot + 1, letter)
+            })
+            x
+        }
+    }
+
+    fun pressButton(button: Char): String {
+        return pressButton2(0, button).minBy { it.length }
+    }
+
+    return input.lines.sumOf { line ->
+        val movements = line.map { pressButton(it) }
+        val m2 = movements.joinToString("")
+        line.dropWhile { it == '0' }.dropLast(1).toInt() * m2.length
+    }.toLong()
 }
 
 fun run2(input: InputData): ResultType {
-    return input.lines.count()
+    val robots = 26
+
+    val robotArms = (0 until robots).map { 'A' }.toMutableList()
+
+    val moves = mapOf(
+        ('A' to 'A') to "A",
+        ('A' to '<') to "v<<A", // "<v<A" // 1
+        // ('A' to '<') to "<v<A",        // 2
+        // ('A' to 'v') to "v<A", // "<vA" // 3
+        ('A' to 'v') to "<vA",            // 4
+        ('A' to '^') to "<A",
+        ('A' to '>') to "vA",
+        // ('<' to 'A') to ">^>A", // ">>^A" // 5
+        ('<' to 'A') to ">>^A",           // 6
+        ('<' to '<') to "A",
+        ('<' to 'v') to ">A",
+        ('<' to '^') to ">^A",
+        ('<' to '>') to ">>A",
+        ('v' to 'A') to "^>A", // ">^A"   // 7
+        // ('v' to 'A') to ">^A",            // 8
+        ('v' to '<') to "<A",
+        ('v' to 'v') to "A",
+        ('v' to '^') to "^A",
+        ('v' to '>') to ">A",
+        ('^' to 'A') to ">A",
+        ('^' to '<') to "v<A",
+        ('^' to 'v') to "vA",
+        ('^' to '^') to "A",
+        // ('^' to '>') to ">vA", // "v>A"     // 9
+        ('^' to '>') to "v>A",              // 10
+        ('>' to 'A') to "^A",
+        ('>' to '<') to "<<A",
+        ('>' to 'v') to "<A",
+        ('>' to '^') to "<^A", // "^<A"     // 11
+        //('>' to '^') to "^<A",              // 12
+        ('>' to '>') to "A",
+    )
+
+    val directions = listOf((1 to 0) to ">", (0 to 1) to "v", (-1 to 0) to "<", (0 to -1) to "^")
+
+    val navigationCache = mutableMapOf<Pair<Char, Char>, List<String>>()
+    fun navigate(start: Char, end: Char): List<String> {
+        return navigationCache.getOrPut(start to end) {
+            val costs = codeKeypad.values.associateWith { Int.MAX_VALUE }.toMutableMap()
+            val ways = mutableMapOf<Pair<Int, Int>, Set<String>>()
+            val queue = PriorityQueue<Pair<Int, Int>> { p1, p2 -> costs[p1]!! - costs[p2]!! }
+            val startPos = codeKeypad[start]!!
+            queue.add(startPos)
+            costs[startPos] = 0
+            ways[startPos] = setOf("")
+            while (queue.isNotEmpty()) {
+                val pos = queue.remove()
+                val cost = costs[pos]!! + 1
+                val way = ways[pos]!!
+                directions.map { (pos.first + it.first.first to pos.second + it.first.second) to it.second }
+                    .filter { costs.containsKey(it.first) }
+                    .forEach { (nextPos, direction) ->
+                        val nextCost = costs[nextPos]!!
+                        if (cost > nextCost) return@forEach
+                        if (cost < nextCost) {
+                            costs[nextPos] = cost
+                            ways[nextPos] = way.map { it + direction }.toSet()
+                            queue.add(nextPos)
+                        } else {
+                            ways[nextPos] = ways[nextPos]!! + way.map { it + direction }.toSet()
+                        }
+                    }
+
+            }
+            val endWays = ways[codeKeypad[end]!!]!!.toList()
+            endWays
+        }
+    }
+
+    val buttonCache = mutableMapOf<Pair<Int, Pair<Char, Char>>, Long>()
+    fun pressButton2(robot: Int, button: Char): Long {
+        if (robot == robots) return 1L
+        val position = robotArms[robot]
+        robotArms[robot] = button
+        return buttonCache.getOrPut(robot to (position to button)) {
+            val movements = moves[position to button] ?: throw RuntimeException("Wrong move: $position to $button")
+            movements.sumOf { m ->
+                pressButton2(robot + 1, m)
+            }
+        }
+    }
+
+    fun pressButton(button: Char): Long {
+        val position = robotArms[0]
+        val movements =
+            navigate(position, button).map { it + "A" }
+        robotArms[0] = button
+
+        return movements.minOf { movement ->
+            movement.sumOf { letter ->
+                pressButton2(1, letter)
+            }
+        }
+    }
+
+    return input.lines.sumOf { line ->
+        val movements = line.map { pressButton(it) }
+        val m2 = movements.sum()
+        line.dropWhile { it == '0' }.dropLast(1).toInt() * m2
+    }
 }
 
 fun main() {
